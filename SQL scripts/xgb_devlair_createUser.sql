a8bd7919cb216febd7d84eafe86a4b7c9d51f9f2
kirill85 <kirillshkrogalev@mail.ru>
2011-08-01 12:43:13 +0400

    reload script: step 2

---
+++ SQL scripts/xgb_devlair_createUser.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Contacts
+(
+	email                varchar(50) NOT NULL,
+	passwd            text NOT NULL,
+	visibleEmail         text NULL,
+	JID                  text NULL,
+	ICQ                  text NULL,
+	MSN 		     text null
+);
+
+alter table Contacts
+add primary key(gUserId, email, passwd);
+
+CREATE TABLE Users (gUserId serial not null,  Nick text not null, 
+email text not null, passwd text not null,
+photo bytea null);
+
+alter table Users
+add primary key(gUserId, email, passwd);
+
+alter table Contacts
+add constraint Users_Contacts foreign key(gUserId, email, passwd)
+references Users(gUserId, email, passwd);
