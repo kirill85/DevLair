bca4c563c25c06333a16d1c26021e77e8ea2c05f
kirill85 <kirillshkrogalev@mail.ru>
2011-08-02 13:23:32 +0400

    Create ORM-wrapped class Users

--- DevLair/DevLair.ORM/Model/Tables/Users.cs
+++ DevLair/DevLair.ORM/Model/Tables/Users.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using NpgsqlTypes;
+using Npgsql;
 
 namespace DevLair.ORM.Model.Tables
 {
-    class Users
+    [Table(Name = "Users")]
+    public class Users
     {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, AutoSync = AutoSync.OnInsert)]
+        public int clientId { get; set; }
+        [Column(Name = "nick")]
+        public string NickName { get; set; }
+        [Column(Name = "passwd", CanBeNull = false)]
+        public string Password { get; set; }
+        [Column(Name = "email")]
+        public string MailTo { get; set; }
+        [Column(Name = "photo")]
+        public byte[] UserPhoto { get; set; }
     }
 }
