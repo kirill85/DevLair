021bb07642de68d4944310f03939c7f39ffa90a6
kirill85 <kirillshkrogalev@mail.ru>
2011-08-05 22:54:04 +0400

    Initial work to wrap table Contacts

---
+++ DevLair/DevLair.ORM/Model/Tables/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+
+namespace DevLair.ORM.Model.Tables
+{
+    [Association(IsForeignKey = true)]
+    [Table(Name = "contacts")]
+    public class Contacts
+    {
+
+    }
+}
