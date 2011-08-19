42e1e9b7683970e3d7cdfa10bb4663bcbcf38a4c
nCdy <X@nCdy.org>
2011-08-19 09:20:10 +0300

    tests update

--- DevLair/DevLair.Tests/DataBase/ConnectionTest.cs
+++ DevLair/DevLair.Tests/DataBase/ConnectionTest.cs
@@ -13,8 +13,10 @@ namespace DevLair.Tests.DataBase
     [TestClass]
     public class ConnectionTest
     {
-        private Configuration config;
+        private String connection = null;
+
         [TestMethod]
+        [TestInitialize]
         public void Connect()
         {
             Encrypter encrypter = new Encrypter("64bit");
@@ -24,11 +26,19 @@ namespace DevLair.Tests.DataBase
             // Check if crypter is changed
             Assert.AreEqual(true, encrypter.SetCrypter("AES"));
 
-            String connection = encrypter.Decrypt
+            this.connection = encrypter.Decrypt
                 ("Sf+ulELX4tNuvsQtUG2EZWPJDPhm8obnrSyEm7F5mtg/eziYSjoIZndeCHk8iZOhvAuMNJBxiiatrRHTt2LVFv/vJNoo8yCHkOO7TwUWv3+l8+Wpis4TSsEvc8zHgEPIucH/OQ2tyYlMboyoFH26dZLZ2Y+Kevfu79VfqLBcKGE=", keyword);
+        }
 
-            config = Fluently.Configure().Database(PostgreSQLConfiguration.Standard.ConnectionString(connection)).BuildSessionFactory();
-            Assert.IsNotNull(config);
+        [TestMethod]
+        public void NhibernateFluentTest()
+        {
+            Assert.IsNotNull(this.connection);
+
+            NHibernate.ISessionFactory session =
+                    Fluently.Configure().Database(PostgreSQLConfiguration.Standard.ConnectionString(this.connection)).BuildSessionFactory();
+
+            Assert.IsNotNull(session);
         }
     }
 }
