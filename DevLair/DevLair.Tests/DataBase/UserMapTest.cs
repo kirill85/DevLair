9ee46ae456bdd5e9bb36c3837d2fc2ecf84a4116
nCdy <X@nCdy.org>
2011-08-30 11:44:43 +0400

    formatting

--- DevLair/DevLair.Tests/DataBase/UserMapTest.cs
+++ DevLair/DevLair.Tests/DataBase/UserMapTest.cs
@@ -32,7 +32,13 @@ namespace DevLair.Tests.DataBase
         {
             Assert.IsNotNull(testConfig.Session);
 
-            var spec = new PersistenceSpecification<Users>(testConfig.Session).CheckProperty(ch => ch.UserId, 1).CheckProperty(ch => ch.NickName, "JonhDoe").CheckProperty(ch => ch.Password, "secret").CheckProperty(ch => ch.MailTo, "jonh@doe.usr").CheckProperty(ch => ch.UserPhoto, null).VerifyTheMappings();
+            var spec = new PersistenceSpecification<Users>
+                (testConfig.Session).CheckProperty(ch => ch.UserId, 1)
+                    .CheckProperty(ch => ch.NickName, "JonhDoe")
+                    .CheckProperty(ch => ch.Password, "secret")
+                    .CheckProperty(ch => ch.MailTo, "jonh@doe.usr")
+                    .CheckProperty(ch => ch.UserPhoto, null)
+                    .VerifyTheMappings();
 
             Assert.IsNotNull(spec);
         }
